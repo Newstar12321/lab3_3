e619ec1847c9e800cd3b204995f67f72b0c1775a
Newstar12321 <tazeev.zhenya@mail.ru>
2023-10-10 01:18:28 +0700

    Добавьте файлы проекта.

---
+++ lab3_3/lab3_3.cpp
@@ -0,0 +1,26 @@
+﻿#define _USE_MATH_DEFINES
+#include <iostream>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    const double a = 0.003, b = 2.5;
+    int x;
+    cout << "Введите переменную x: ";
+    cin >> x;
+    if (x <= -6)
+    {
+        cout << "y = " << pow(M_E, x) + pow(x, 2);
+    }
+    else if (x > 5)
+    {
+        cout << "y = " << (1 + a) / (1 + x);
+    }
+    else
+    {
+        cout << "y = " << pow(M_LN10, 2) * abs(b * x);
+    }
+}
